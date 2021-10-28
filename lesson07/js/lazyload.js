e0a10dba36a0dc18fe19e59a2e95beb737c4d10b
Eddie McConkie <edwardmcconkie@gmail.com>
2021-10-28 12:19:44 -0600

    Worked on week 7 lazyload

---
+++ lesson07/js/lazyload.js
@@ -0,0 +1,40 @@
+const images = document.querySelectorAll('[data-src]')
+
+const preloadImage = (image) => {
+  const src = image.getAttribute('data-src')
+  if (!src) {
+    return
+  } else {
+    image.src = src
+    image.onload = () => {
+      image.removeAttribute('data-src')
+    }
+  }
+}
+
+const imageOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px 300px 0px'
+}
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return
+      } else {
+        const image = entry.target
+        preloadImage(image)
+        observer.unobserve(image)
+      }
+    })
+  }, imageOptions)
+
+  images.forEach((image) => {
+    observer.observe(image)
+  })
+} else {
+  images.forEach(function (image) {
+    image.src = image.dataset.src
+  })
+}
